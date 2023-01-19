624d3e634966d381aed3312075e4a3a91ced3780
Karsten Koerner <kgk1999@gmail.com>
2023-01-19 02:28:34 -0500

    search algorithm done

---
+++ src/functions/searchAlgo.js
@@ -0,0 +1,52 @@
+import { doc, setDoc, getDoc, collection, where, query, getDocs } from 'firebase/firestore'
+import { db, auth, user } from '../firebase/config'
+import { useEffect, useState, lazy } from 'react'
+
+
+
+
+
+// Search algorithm for best matching posts
+const searchAlgo = async (input) => {
+
+    let searchResults = []
+    let searchWords = searchTerm.split(" ")
+
+    const postsRef = collection(db, "posts")
+    const q = query(postsRef)
+    const querySnapshot = await getDocs(q)
+        if (!querySnapshot.empty) {
+            querySnapshot.forEach(function(doc) {
+                let item = doc.data()
+                item.score = 0
+    
+                for (let i = 0; i < searchWords.length; i++) {
+                    let searchWord = searchWords[i]
+    
+                    if (item.title.includes(searchWord)) {
+                        item.score += 1
+                    }
+                    if (item.description.includes(searchWord)) {
+                        item.score += 0.5
+                    }
+                    if (item.tags.includes(searchWord)) {
+                        item.score += 0.8
+                    }
+                }
+                if (item.score > 0) {
+                    searchResults.push(item)
+                }
+            })
+        } else {
+            console.log("No posts matching your search.")
+        }
+
+        searchResults.sort((a, b) => {
+            return b.score - a.score
+        })
+        
+        displaySearchResults(searchResults)
+}
+
+
+export default searchAlgo
